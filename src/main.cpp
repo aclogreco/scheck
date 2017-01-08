26cdc3b11c778c61b85db6b4b90b87e9b9e3297c
Anthony LoGreco <aclogreco@gmail.com>
2017-01-07 20:47:27 -0500

    Get input from the user and check it against the word list.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@ using namespace std;
 int main() {
     cout << "scheck version 0.1" << endl;
     Dictionary d("data/mydict.dat");
-    string word = "dog";
+    string word;
     
-    if (d.Check(word)) {
-        cout << word << " is OK\n";
-    }
-    else {
-        cout << word << " is misspelt \n";
+    while (getline(cin, word)) {
+        if (d.Check(word)) {
+            cout << word << " is OK\n";
+        }
+        else {
+            cout << word << " is misspelt \n";
+        }
     }
 }
