e42ba73aad826977a28c45d64d94aa1be3a1f8d1
Anthony LoGreco <aclogreco@gmail.com>
2017-01-08 12:33:08 -0500

    Add an input test data file. Open the input test file and pass it to the Parser in main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <string>
 #include "dictionary.h"
+#include "parser.h"
 
 using namespace std;
 
 int main() {
     try {
-        cout << "scheck version 0.1" << endl;
+        cout << "scheck version 0.4" << endl;
 
-        //Dictionary d("data/mydict.dat");
+        Dictionary d("data/mydict.dat");
         //Dictionary d("data/not-there.dat");
-        Dictionary d("data/dict.dat");
-
+        //Dictionary d("data/dict.dat");
+        
+        ifstream sub("data/sub1.txt");
+        if (!sub.is_open()) {
+            throw ScheckError("cannot open data/sub1.txt");
+        }
+        
+        
+        Parser p(sub);
+        
+        
         string word;
         
-        while (getline(cin, word)) {
+        while ((word = p.NextWord()) != "") {
             if (d.Check(word)) {
                 cout << word << " is OK\n";
             }
