223ea5327ec945e0b55090bea64f47c41beb447b
Anthony LoGreco <aclogreco@gmail.com>
2017-01-07 21:46:27 -0500

    Add error exception class. Use this class to throw exceptions for errors in the dictionary class.

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -11,10 +11,20 @@ class Dictionary {
     public:
         Dictionary( const std::string & fname ) {
             std::ifstream wlist(fname.c_str());
+            if (!wlist.is_open()) {
+                // report open error
+                throw ScheckError("Could not open dictionary file " + fname);
+            }
+            
             std::string word;
 
             while (std::getline(wlist, word)) {
                 mWords.insert(word);
+            }
+
+            if (!wlist.eof()) {
+                // report read error
+                throw ScheckError("Error reading dictionary file " + fname);
             }
         }
 
