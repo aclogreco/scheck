1801da4f9f5a209c39c570f5b293d5d414a4686a
Anthony LoGreco <aclogreco@gmail.com>
2017-01-12 16:31:26 -0500

    Add declaration for Settings class.

---
+++ inc/settings.h
@@ -0,0 +1,26 @@
+#ifndef INC_SETTINGS_H
+#define INC_SETTINGS_H
+
+
+#include <string>
+#include "cmdline.h"
+
+
+class Settings {
+
+    public:
+
+        enum Report { rtCSV, rtXML };
+        Settings (CmdLine &c1);
+        Report ReportType() const;
+        std::string DictName() const;
+
+    private:
+
+        Report mRepType;
+        std::string mDictName;
+
+};
+
+
+#endif
